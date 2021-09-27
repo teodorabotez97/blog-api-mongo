c8a5ac2e25a903c64129f2ddd7498e4909f73417
Teodora Botez <teodora.botez@crystal-system.eu>
2021-09-27 16:18:39 +0300

    Update ArticleRepository.java

--- src/main/java/com/teodora/blog/repository/ArticleRepository.java
+++ src/main/java/com/teodora/blog/repository/ArticleRepository.java
@@ -2,12 +2,15 @@ package com.teodora.blog.repository;
 
 import com.teodora.blog.model.Article;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 
 public interface ArticleRepository extends MongoRepository<Article, Integer> {
     List<Article> findByTitleContaining(String title);
+    
+    @Query("Select a from Article a  where a.title like ?1%")
     List<Article> getArticleByTitle(String title);
 
 }
